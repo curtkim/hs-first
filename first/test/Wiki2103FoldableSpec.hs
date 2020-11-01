cdb7917f9989d180070db20823f76b2ee6e827e2
curt <curt.k@kakaomobility.com>
2020-11-01 17:13:47 +0900

    Debug.Trace 예제추가

--- first/test/Wiki2103FoldableSpec.hs
+++ first/test/Wiki2103FoldableSpec.hs
@@ -3,7 +3,10 @@ module Wiki2103FoldableSpec where
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
+import Debug.Trace
 
+
+plusplus x y = trace ("call plusplus x="++ show x ++ " y=" ++ show y) (x ++ y)
 
 
 spec :: Spec
@@ -11,6 +14,15 @@ spec = do
 
   it "foldr" $ do       
     foldl (+) 0 [1,2,3] `shouldBe` 6
-    foldr (++) "" ["a", "b", "c"] `shouldBe` "abc"
+
+    -- call plusplus x="c" y=""
+    -- call plusplus x="b" y="c"
+    -- call plusplus x="a" y="bc"
+    foldr plusplus "" ["a", "b", "c"] `shouldBe` "abc"
+    
+    -- call plusplus x="" y="a"
+    -- call plusplus x="a" y="b"
+    -- call plusplus x="ab" y="c"    
+    foldl plusplus "" ["a", "b", "c"] `shouldBe` "abc"
 
     mconcat ["Tree", "fingers"] `shouldBe` "Treefingers"
